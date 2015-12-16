1bc73e6fc490102edbe9b551bcbafd8b4b27c5e5
kaiery <kaiery@qq.com>
2015-12-16 20:58:47 +0800

    werew

--- Test/mobilesafe/src/main/java/com/fanzhang/mobilesafe/activites/SplashActivity.java
+++ Test/mobilesafe/src/main/java/com/fanzhang/mobilesafe/activites/SplashActivity.java
@@ -1,15 +1,27 @@
 package com.fanzhang.mobilesafe.activites;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 import com.fanzhang.mobilesafe.R;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
+
+    private ImageView iv_splash_activity_logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // 使屏幕不显示标题栏(必须要在setContentView方法执行前执行)
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // 隐藏状态栏，使内容全屏显示(必须要在setContentView方法执行前执行)
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_activity);
+
+
+        iv_splash_activity_logo = (ImageView) findViewById(R.id.iv_splash_activity_logo);
     }
 }
